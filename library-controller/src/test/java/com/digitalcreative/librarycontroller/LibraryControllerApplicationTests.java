ad0fd96fd75e4c9f57fa6139ae42d751da4f94d2
Anthony Pereira <pereira_anthony@icloud.com>
2021-01-10 21:42:51 +0100

    Library application start

---
+++ library-controller/src/test/java/com/digitalcreative/librarycontroller/LibraryControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalcreative.librarycontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ library-model/src/main/java/com/digitalcreative/librarymodel/LibraryModelApplication.java
@@ -0,0 +1,13 @@
+package com.digitalcreative.librarymodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryModelApplication.class, args);
+	}
+
+}
