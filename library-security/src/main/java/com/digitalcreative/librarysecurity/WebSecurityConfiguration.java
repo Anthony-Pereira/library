1d2d85d86a74ea73fd02663a254d1a124a20c876
Anthony Pereira <pereira_anthony@icloud.com>
2022-02-28 15:32:24 +0100

    add oauth2 dependency

--- library-security/src/main/java/com/digitalcreative/librarysecurity/WebSecurityConfiguration.java
+++ library-security/src/main/java/com/digitalcreative/librarysecurity/WebSecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,6 +20,7 @@ import org.springframework.stereotype.Component;
 @Data
 @Component
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
@@ -37,13 +39,21 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated();
-        http.formLogin().loginPage("/signIn");
-        http.formLogin();
-        http.csrf().disable();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.addFilter(new JWTAuthenticationFilter(authenticationManager()));
-        http.addFilterBefore(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        http
+                .authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasRole("USER")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin();
+        
+        //http.authorizeRequests().anyRequest().authenticated();
+        //http.formLogin().loginPage("/signIn");
+        //http.csrf().disable();
+        //http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        //http.addFilter(new JWTAuthenticationFilter(authenticationManager()));
+        //http.addFilterBefore(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
